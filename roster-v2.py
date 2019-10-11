f1408ca46c3d1e3e92eee05c5e5928f612f30832
skb30 <sbarth@gmail.com>
2019-10-11 07:03:53 -0700

    merge rosters

--- roster-v2.py
+++ roster-v2.py
@@ -21,119 +21,63 @@ import csv
     #
     #
     #
-    #
-    #
+    # "Paul 3", "Paul 4"
+    #["Edward 1","Edward 2","Edward 3","Edward 3","Edward 5"],
 
-
-
-def init_posts(shift):
-
-    days = {
-    "Gates" : {
-        "post" : ["Charlie 1","Charlie 2","Charlie 3"],
-        "fill" : "3"
-
+rosters = {
+    "day_priority_roster" : {
+        "Gates" : {
+            "Charlie 1" : "Vacant",
+            "Charlie 2" : "Vacant",
+            "Charlie 3" : "Vacant"
+        },
+        "Booths": {
+            "Wofle 1" : "Vacant",
+            "Wofle 2" : "Vacant",
+            "Tantau 1" : "Vacant"
+        },
+        "Mobiles" : {
+            "Paul 1" : "Vacant"
+        }
     },
-    "Booths": {
-        "post" : ["Wofle 1","Wofle 2","Tantau 1"],
-        "fill" : "3"
-    },
-    "Mobiles" : {
-        "post" : ["Paul 1", "Paul 2", "Paul 3", "Paul 4"],
-        "fill" : "2"
-    },
-    "Edwards": {
-        "post" : ["Edward 1","Edward 2","Edward 3","Edward 3","Edward 5"],
-        "fill" : "0"
+    "day_roster" : {
+        "Booths": {
+            "Wofle 3" : "Vacant",
+            "Tantau 2" : "Vacant"
+        },
+        "Mobiles" : {
+            "Paul 2" : "Vacant",
+            "Paul 3" : "Vacant",
+            "Paul 4" : "Vacant"
+        },
+        "Edwards" : {
+            "Edward 1" : "Vacant",
+            "Edward 2" : "Vacant",
+            "Edward 3" : "Vacant",
+            "Edward 4" : "Vacant",
+            "Edward 5" : "Vacant"
+        }
     }
-
-}
-    swing = {
-    "Mobiles" : {
-        "Paul 1" : "Vacant",
-        "Paul 2" : "Vacant",
-        "Paul 3" : "Vacant",
-        "1" : "ID"
-    },
-    "Gates" : {
-        "Charlie 1": "Vacant",
-        "Charlie 2": "Vacant",
-        "Charlie 3": "Vacant",
-        "5D": "Vacant",
-        "1C": "Vacant",
-        "7D": "Vacant",
-        "2": "ID"
-    },
-    "Booths": {
-        "Wofle 1": "Vacant",
-        "Wofle 2": "Vacant",
-        "Wofle 3": "Vacant",
-        "Tantau 1": "Vacant",
-        "Tantau 2": "Vacant",
-        "3": "ID"
-    },
-    "Breakers": {
-        "Edward 1": "Vacant",
-        "Edward 2": "Vacant",
-        "Edward 3": "Vacant",
-        "Edward 4": "Vacant",
-        "Edward 5": "Vacant",
-        "Edward 6": "Vacant",
-        "Edward 7": "Vacant",
-        "Edward 8": "Vacant",
-        "4": "ID"
-    }
-}   
-    return days
-def create_roster(staff, posts):
-
+}  
+    
+    
+def create_rosters(staff, roster):
     shuffle(staff)
-    must_fill = {}
-    must_fill_roaster = []
-    
+    # fill the rosters
 
-    # print(posts["Gates"]["post"][1])
-
-    # get the group of positions 
-    position_types = list(posts.keys()) 
-    shuffle(position_types)
-    
-    staffCount = 0
-    # create the must_fill hat
-    for position_type in position_types:
-        must_fill[position_type] = posts[position_type]["fill"]
-
-    # process the "must-fill" hat
-     
-    for position_type, numberToFill in must_fill.items():
-        x = int(numberToFill)
-        position  = posts[position_type]["post"]
-    #     print("position_type {} numberToFill {} ".format(position_type, numberToFill))
-        # get the positions for this type
-        # positions = posts[position_type]["post"]
-        j = 0
-
-        must_fill_roaster.append(position_type)
-        while x != 0: 
-            must_fill_roaster.append({position[j] : staff.pop()}) 
-            j += 1
-            # staffCount += 1
-            x -= 1
-        # j = 0
-        # if staffCount < len(staff):
-        #     for position in positions:
-        #         posts[position_type][position[j]] = staff.pop()
-        #         staffCount += 1
-        #         j += 1
-        # else:
-        #     print("No more staff.")
-        # print(x, y)
-
-    return posts
+    shift_roster = rosters[roster]
+    for position_type in shift_roster:
+        for position in shift_roster[position_type]:
+            if len(staff) != 0:
+                shift_roster[position_type][position] = staff.pop()
+            else:
+                print("No more staff")
+                return roster
+    return roster
 
 def create_list(filename):
     filelist = []
- 
+
     with open(filename, newline='') as csvfile:
         shiftsRoaster = csv.reader(csvfile, delimiter=',', quotechar='|')
         for name in shiftsRoaster:
@@ -152,12 +96,15 @@ def write_roster(roster_file, roster):
 
 def main():
     # constructor 
-    dayShift = init_posts("Day")
+    # dayShiftPriority = init_posts("Day")
     staff = create_list("small-input.csv")
     # build the post list
-    roster = create_roster(staff, dayShift)
+    day_priority_roster = create_rosters(staff,"day_priority_roster")
+    if len(staff) > 0:
+        day_roster = create_rosters(staff,"day_roster")
 
-    write_roster("roster-day.csv", roster)
+    write_roster("day-p1-roster.csv", day_priority_roster)
+    write_roster("day-p2-roster.csv", day_roster)
     
     print("done")
 
