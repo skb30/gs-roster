46a8b865e53d73bc13e0f95322ecff63b382076d
Sccott B <scott.barth@asg.comgit config --global user.name Scott>
2019-10-19 16:39:51 -0700

    working version 1

--- roster-v2.py
+++ roster-v2.py
@@ -38,12 +38,12 @@ roster_policy  = [
         {
             "name" :  "swing",
             "groups" :  [ {
-                "name" : "Gates", 
-                "mustFillPosts" : ["Charlie 1","Charlie 2","Charlie 3"],
+                "name" : "Georges", 
+                "mustFillPosts" : ["George 1","George 2","George 3","George 4","George 5","George 6","George 7"],
                 "overflowPosts" : []
             } , {
-                "name" : "Booths", 
-                "mustFillPosts" : ["Wofle 1","Wofle 2","Tantau 1"],
+                "name" : "Roberts", 
+                "mustFillPosts" : ["Robert 1","Robert 2"],
                 "overflowPosts" : ["Tantau 2"]
             } , { 
                 "name" : "Mobiles" , 
@@ -51,8 +51,8 @@ roster_policy  = [
                 "overflowPosts" : ["Paul 3", "Paul 4","Paul 5"]
             } , {
                 "name" : "Edwards", 
-                "mustFillPosts" : ["Scott 1"],
-                "overflowPosts" : ["Edward 1","Edward 2","Edward 3","Edward 4","Edward 5"]
+                "mustFillPosts" : ["Edward 1"],
+                "overflowPosts" : ["Edward 2","Edward 3","Edward 4","Edward 5","Edward 6"]
             } ]
         } , 
         {
@@ -85,22 +85,20 @@ def get_blank_rosters(shift_roster, roster, priority):
     blank_roster = []
 
     for shift in roster[0]["shift"]:
-        print(shift["name"])
         i = 0
-        if shift["name"] == 'day':
-            
+        if shift["name"] == shift_roster:
             posts = shift["groups"]
-            # print(shiftName.upper())
+            
             for post in posts:
                 mustFillPosts = post[fill]
                 size = len(mustFillPosts)
                 if size != 0:
-                    blank_roster.append([]) # create a row 
+                    blank_roster.append([]) # create a roster row 
                     size -= 1
                 
                 for unit in mustFillPosts:
-                    blank_roster[i].append(unit)
-                    blank_roster[i].append("Vacant")
+                    blank_roster[i].append(unit) # add unit column to the row
+                    blank_roster[i].append("Vacant") # add name column to the row
                     i += 1
                     if size != 0:
                         blank_roster.append([])
@@ -119,29 +117,68 @@ def get_staff(filename):
     return filelist
 def write_roster(roster_file, roster):
     with open(roster_file, 'w') as f:
+
+
         f.write("%s,%s\n" % ("POST","NAME"))
-        for position in roster.keys():
-            f.write("%s\n" %position)
-            for unit in roster[position]:
-                f.write("%s,%s\n" % (unit, roster[position][unit]))
+        for position in roster:
+            f.write("%s,%s\n" % (position[0],position[1]))
 
 
 def main():
-    staff = get_staff("large-input.csv")
-
     print("*** Start ***")
-    p = get_blank_rosters("day", roster_policy, 1)
-    for item in p:
-        print(item)
-    # get the must fill positions for day shift
 
-
-
-    print("*** End ***")
-    # create_rosters(staff, roster_policy, 2)
-
+    # staff = get_staff("really-small-input.csv")
+    # staff = get_staff("small-input.csv")
+    staff = get_staff("large-input.csv")
+    shuffle(staff)
+    mustFillRoster = get_blank_rosters("day", roster_policy, 1)
+    overFlowRoster = get_blank_rosters("day", roster_policy, 2)
     
-    print("*** done ***")
+    row            = 0
+    fullRoster     = []
+    # sizeOfStaff    = len(staff)
+    sizeOfMustFill = len(mustFillRoster)
+    sizeOfOverFlow = len(overFlowRoster)
+    combined       = sizeOfMustFill + sizeOfOverFlow
+
+    if len(staff) <= len(mustFillRoster): # More positions to fill than staff so we'll fill the must-fill
+        shuffle(mustFillRoster)
+
+        while len(staff) > 0:
+            mustFillRoster[row][1] = staff.pop()
+            row += 1
+        mustFillRoster.sort()
+        fullRoster = mustFillRoster + overFlowRoster
+
+    elif len(staff) <= combined: 
+            while sizeOfMustFill > 0:
+                mustFillRoster[row][1] = staff.pop()
+                row += 1
+                sizeOfMustFill -= 1
+            row = 0 
+    
+            if len(staff) >= 0:
+                while sizeOfOverFlow > 0 and len(staff) > 0:
+                    overFlowRoster[row][1] = staff.pop()
+                    row += 1
+                    sizeOfOverFlow -= 1    
+                
+            fullRoster = mustFillRoster + overFlowRoster
+    else:
+        # We have more staff than positions
+        fullRoster = mustFillRoster + overFlowRoster 
+        shuffle(fullRoster)
+        sizeOfFullRoster = len(fullRoster)
+        while sizeOfFullRoster > 0:
+            fullRoster[row][1] = staff.pop()
+            row += 1
+            sizeOfFullRoster -= 1
+        fullRoster.sort()    
+
+    write_roster("new-roster.csv", fullRoster) 
+    for p in fullRoster:
+        print("{},{}".format(p[0], p[1]))    
+    print("*** End ***")
 
 if __name__ == "__main__":
   main()
